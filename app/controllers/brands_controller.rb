95aa9cdec3f733d810c9be1087ac2b7142cf869e
madoka kimizuka <kimitaro0415@gmail.com>
2019-08-18 20:49:47 +0900

    feat: brand に関してCRUD,view を実装

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,4 +1,20 @@
 class BrandsController < ApplicationController
+  before_action :set_brand, only: %i[show edit update destroy]
+
   def index
+    @brands = Brand.all
+  end
+
+  def show
+  end
+
+  private
+
+  def brand_params
+    params.require(:brand).permit(:name, :founder, :country, :concept, :found_year, :official)
+  end
+
+  def set_brand
+    @brand = Brand.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
     resources :fragrances, except: [:show]
-    resources :brands
+    resources :brands, except: [:show]
     resources :perfumers
   end
   devise_for :users
@@ -10,8 +10,7 @@ Rails.application.routes.draw do
   root to: 'notes#index'
   resources :notes, except: :index
 
-  resources :fragrances
-  # , only: [:index, :show]
-  # resources :brands
+  resources :fragrances, only: [:index, :show]
+  resources :brands, only: [:index, :show]
   # resources :perfumers
 end
