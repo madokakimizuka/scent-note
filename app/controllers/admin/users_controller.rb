dc23d734620496702e74b13423af942f717619af
madoka kimizuka <kimitaro0415@gmail.com>
2019-08-21 15:25:25 +0900

    feat:ユーザー管理機能を実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,12 +4,31 @@ class Admin::UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました"
+    else
+      render 'edit', notic: "管理者がいなくなってしまいます。"
+    end
+  end
+
   def show
+    @notes = @user.notes.all
   end
 
   def index
@@ -28,5 +47,9 @@ class Admin::UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end
